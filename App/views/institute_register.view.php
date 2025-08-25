93298c878bbda9f21c3d004fd7dd5d835f3a1c0d
Dilana Deepika <dilanasuwendra@gmail.com>
2025-08-25 14:09:02 +0530

    add variable.css file and modife the home page

--- App/views/institute_register.view.php
+++ App/views/institute_register.view.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Institute Registration</title>
-  <link rel="stylesheet" href="asset/css/institute_register.css">
+  <link rel="stylesheet" href="../../Public/assets/css/institute_register.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
